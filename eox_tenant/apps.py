30aacebf20685810d344077817c93cd277e91cd4
Squirrel18 <daniel.quiroga@edunext.co>
2018-11-07 16:32:57 -0500

    Add plugin settings.

--- eox_tenant/apps.py
+++ eox_tenant/apps.py
@@ -3,15 +3,18 @@ File configuration for eox-tenant.
 """
 from django.apps import AppConfig
 
-class EdunextOpenEdxExtensionsConfig(AppConfig):
+class EdunextOpenEdxExtensionsTenantConfig(AppConfig):
     """
+    App configuration
     """
     name = 'eox_tenant'
     verbose_name = "Edunext Open edx extensions tenant."
 
     plugin_app = {
         'settings_config': {
-            'lms.djangoapp': {},
-            'cms.djangoapp': {},
+            'lms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'aws': {'relative_path': 'settings.aws'},
+            },
         },
     }
