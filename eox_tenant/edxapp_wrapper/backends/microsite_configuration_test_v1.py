dd667a02d800b2ced1ef6a66039fdec2952511f9
Felipe Montoya <felipe.montoya@edunext.co>
2018-11-23 15:14:33 -0500

    Removing the dependency from microsite_configuration in tests to make them run standalone

---
+++ eox_tenant/edxapp_wrapper/backends/microsite_configuration_test_v1.py
@@ -0,0 +1,35 @@
+""" Backend abstraction. """
+
+from eox_tenant.backends.base import AbstractBaseMicrositeBackend
+
+
+def get_base_microsite_backend():
+    """ Backend to get BaseMicrositeBackend. """
+    try:
+        from microsite_configuration.backends.base import BaseMicrositeBackend as InterfaceConnectionBackend
+    except ImportError:
+        InterfaceConnectionBackend = AbstractBaseMicrositeBackend
+    return InterfaceConnectionBackend
+
+def get_base_microsite_template_backend():
+    """ Backend to get BaseMicrositeTemplateBackend. """
+    try:
+        from microsite_configuration.backends.base import BaseMicrositeTemplateBackend
+    except ImportError:
+        from eox_tenant.backends.base import BaseMicrositeTemplateBackend
+    return BaseMicrositeTemplateBackend
+
+def get_microsite():
+    """ Backend to get microsite. """
+    from microsite_configuration import microsite # pylint: disable=import-error
+    return microsite
+
+def get_microsite_get_value(*args, **kwargs):
+    """ Backend to get get_value. """
+    from microsite_configuration.microsite import get_value
+    return get_value(*args, **kwargs)
+
+def get_is_request_in_microsite():
+    """ Backend to get is_request_in_microsite. """
+    from microsite_configuration.microsite import is_request_in_microsite
+    return is_request_in_microsite
