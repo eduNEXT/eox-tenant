76f3c2ef5478e1969f896dcea5b7d98bb5eaff7a
Andrey Canon <andrey.canon@edunext.co>
2019-12-04 12:56:08 -0500

    Remove EdunextCompatibleFilebasedMicrositeTemplateBackend.

--- eox_tenant/settings/common.py
+++ eox_tenant/settings/common.py
@@ -37,8 +37,6 @@ def plugin_settings(settings):
     """
     # Plugin settings.
     settings.MICROSITE_BACKEND = 'eox_tenant.backends.database.TenantConfigCompatibleMicrositeBackend'
-    settings.MICROSITE_TEMPLATE_BACKEND = \
-        'eox_tenant.backends.filebased.EdunextCompatibleFilebasedMicrositeTemplateBackend'
     settings.MICROSITE_CONFIGURATION_BACKEND = 'eox_tenant.edxapp_wrapper.backends.microsite_configuration_h_v1'
     settings.MICROSITES_ALL_ORGS_CACHE_KEY_TIMEOUT = 300
     settings.GET_BRANDING_API = 'eox_tenant.edxapp_wrapper.backends.branding_api_h_v1'
