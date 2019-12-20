540d594c4a0ba6d7dfd52909a89c0cdd1e03d7b4
Andrey Canon <andrey.canon@edunext.co>
2019-12-20 09:46:17 -0500

    fixing platform tests Verify dependency import and turn off by default.

--- eox_tenant/edxapp_wrapper/backends/certificates_module_i_v1.py
+++ eox_tenant/edxapp_wrapper/backends/certificates_module_i_v1.py
@@ -2,7 +2,11 @@
 Backend Certificates file, here should be all the necessary methods,
 classes and modules from lms.djangoapss.certificates.
 """
+from django.conf import settings
+
 from lms.djangoapps.certificates import models  # pylint: disable=import-error
+
+from eox_tenant.test_utils import TestCertificatesModels
 
 
 def get_certificates_models():
@@ -11,4 +15,4 @@ def get_certificates_models():
 
     Return <lms.djangoapps.certificates.models>.
     """
-    return models
+    return models if getattr(settings, 'USE_EOX_TENANT', False) else TestCertificatesModels()
