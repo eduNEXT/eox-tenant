987f29b1e191246f78da6cd2705c210f38d30eb8
Andrey Canon <andrey.canon@edunext.co>
2019-12-20 11:40:43 -0500

    Add backend comment.

--- eox_tenant/edxapp_wrapper/backends/certificates_module_i_v1.py
+++ eox_tenant/edxapp_wrapper/backends/certificates_module_i_v1.py
@@ -15,4 +15,9 @@ def get_certificates_models():
 
     Return <lms.djangoapps.certificates.models>.
     """
+
+    # The following logic is necessary, because we don't want to use the certificate models as backend,
+    # when we are running the platform test, in order to avoid django migration errors.
+    # USE_EOX_TENANT is set to False by default, which allows to run the platform tests with its normal behavior,
+    # and the plugin tests are run with a different backend.
     return models if getattr(settings, 'USE_EOX_TENANT', False) else TestCertificatesModels()
