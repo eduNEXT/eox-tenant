089ea3350191b218f4aea858c0507b449fe1e219
mariagrimaldi <maria.grimaldi@edunext.co>
2020-05-12 16:43:31 -0400

    added more tests for wrappers to increase coverage

--- eox_tenant/test/test_wrappers.py
+++ eox_tenant/test/test_wrappers.py
@@ -6,9 +6,71 @@ from django.test import TestCase
 from mock import patch, Mock
 
 from eox_tenant.edxapp_wrapper import (
-    site_configuration_module,
+    auth,
+    branding_api,
+    certificates_module,
+    edxmako_module,
     get_common_util,
+    site_configuration_module,
+    users,
 )
+
+
+class AuthTest(TestCase):
+    """
+    Making sure that the auth backend works
+    """
+
+    @patch('eox_tenant.edxapp_wrapper.auth.import_module')
+    def test_imported_module_is_used(self, import_mock):
+        """
+        Testing the backend is imported and used
+        """
+        backend = Mock()
+        import_mock.side_effect = backend
+
+        auth.get_edx_auth_backend()
+
+        import_mock.assert_called()
+        backend.assert_called()
+
+
+class BrandingApiTest(TestCase):
+    """
+    Making sure that the branding API backend works
+    """
+
+    @patch('eox_tenant.edxapp_wrapper.branding_api.import_module')
+    def test_imported_module_is_used(self, import_mock):
+        """
+        Testing the backend is imported and used
+        """
+        backend = Mock()
+        import_mock.side_effect = backend
+
+        branding_api.get_branding_api()
+
+        import_mock.assert_called()
+        backend.assert_called()
+
+
+class CertificatesTest(TestCase):
+    """
+    Making sure that the certificates backend works
+    """
+
+    @patch('eox_tenant.edxapp_wrapper.certificates_module.import_module')
+    def test_imported_module_is_used(self, import_mock):
+        """
+        Testing the backend is imported and used
+        """
+        backend = Mock()
+        import_mock.side_effect = backend
+
+        certificates_module.get_certificates_models()
+
+        import_mock.assert_called()
+        backend.assert_called()
 
 
 class ConfigurationHelpersTest(TestCase):
@@ -30,6 +92,25 @@ class ConfigurationHelpersTest(TestCase):
         backend.assert_called()
 
 
+class EdxMakoTest(TestCase):
+    """
+    Making sure that the edxMako backend works
+    """
+
+    @patch('eox_tenant.edxapp_wrapper.edxmako_module.import_module')
+    def test_imported_module_is_used(self, import_mock):
+        """
+        Testing the backend is imported and used
+        """
+        backend = Mock()
+        import_mock.side_effect = backend
+
+        edxmako_module.get_edxmako_module()
+
+        import_mock.assert_called()
+        backend.assert_called()
+
+
 class UtilsTest(TestCase):
     """
     Making sure that the utils do as they should
@@ -43,3 +124,22 @@ class UtilsTest(TestCase):
             get_common_util.strip_port_from_host('host:8000'),
             'host'
         )
+
+
+class UsersTest(TestCase):
+    """
+    Making sure that the branding API backend works
+    """
+
+    @patch('eox_tenant.edxapp_wrapper.users.import_module')
+    def test_imported_module_is_used(self, import_mock):
+        """
+        Testing the backend is imported and used
+        """
+        backend = Mock()
+        import_mock.side_effect = backend
+
+        users.get_user_signup_source()
+
+        import_mock.assert_called()
+        backend.assert_called()
