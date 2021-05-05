f5bf9764ae23d400d08108e4ad5e5d9dcaf1f2ee
Andrey Canon <andrey.canon@edunext.co>
2021-05-05 16:00:16 -0500

    refactor!: Remove unnecessary migrations.

--- eox_tenant/migrations/0003_auto_20190620_1704.py
+++ eox_tenant/migrations/0003_auto_20190620_1704.py
@@ -10,7 +10,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('eox_tenant', '0002_redirection'),
+        ('eox_tenant', '0001_initial'),
     ]
 
     operations = [
