99d71229a52ee076a11781112face0f243a1479d
black-redoc <joseb.twelve@gmail.com>
2022-04-19 12:26:53 -0500

    feat: added real json data to response

--- eox_tenant/api/v1/views.py
+++ eox_tenant/api/v1/views.py
@@ -1,39 +1,43 @@
-from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from eox_tenant.models import Microsite
+from eox_tenant.models import TenantConfig
 
 
 class MFESettingsView(APIView):
     def get(self, request, format=None, *args, **kwargs) -> Response:
-        return Response(
-            {
-                "id": "localhost",
-                "common": {
-                    "SITE_NAME": "json-server",
-                    "LOGO_URL": "https://www.w3.org/TR/SVG-access/tiger.png",
-                    "LOGO_TRADEMARK_URL": "https://www.w3.org/TR/SVG-access/tiger.png",
-                    "LOGO_WHITE_URL": "https://www.w3.org/TR/SVG-access/tiger.png",
-                    "INFO_EMAIL": "diana.olarte@edunext.co",
-                    "FAVICON_URL": "https://www.w3.org/TR/SVG-access/tiger.png",
-                    "DISCOVERY_API_BASE_URL": "http://localhost:18381",
-                    "PUBLISHER_BASE_URL": "http://localhost:18400",
-                    "ECOMMERCE_BASE_URL": "http://localhost:18130",
-                    "LEARNING_BASE_URL": "http://localhost:2000",
-                    "LMS_BASE_URL": "http://localhost:18000",
-                    "LOGIN_URL": "http://localhost:18000/login",
-                    "LOGOUT_URL": "http://localhost:18000/logout",
-                    "STUDIO_BASE_URL": "http://localhost:18010",
-                    "MARKETING_SITE_BASE_URL": "http://localhost:18000",
-                    "ORDER_HISTORY_URL": "http://localhost:1996/orders",
-                    "REFRESH_ACCESS_TOKEN_ENDPOINT": "http://localhost:18000/login_refresh",
-                    "SEGMENT_KEY": "",
-                    "IGNORED_ERROR_REGEX": "",
-                    "CREDENTIALS_BASE_URL": "http://localhost:18150",
-                },
-                "learning": {},
-                "account": {},
-                "profile": {},
-            }
-        )
+        tenant_key = kwargs["tenant"]
+        tenant = get_object_or_404(TenantConfig, external_key__contains=tenant_key)
+        configs = tenant.lms_configs
+
+        tennat_settings = {
+            "id": configs["LMS_BASE"],
+            "common": {
+                "SITE_NAME": configs["LMS_BASE"],
+                "LOGO_URL": configs["LOGO_URL"],
+                "LOGO_TRADEMARK_URL": configs["LOGO_TRADEMARK_URL"],
+                "LOGO_WHITE_URL": configs["LOGO_WHITE_URL"],
+                "INFO_EMAIL": configs["INFO_EMAIL"],
+                "FAVICON_URL": configs["FAVICON_URL"],
+                "DISCOVERY_API_BASE_URL": configs["DISCOVERY_API_BASE_URL"],
+                "PUBLISHER_BASE_URL": configs["PUBLISHER_BASE_URL"],
+                "ECOMMERCE_BASE_URL": configs["ECOMMERCE_BASE_URL"],
+                "LEARNING_BASE_URL": configs["LEARNING_BASE_URL"],
+                "LMS_BASE_URL": configs["LMS_BASE_URL"],
+                "LOGIN_URL": configs["LOGIN_URL"],
+                "LOGOUT_URL": configs["LOGOUT_URL"],
+                "STUDIO_BASE_URL": configs["STUDIO_BASE_URL"],
+                "MARKETING_SITE_BASE_URL": configs["MARKETING_SITE_BASE_URL"],
+                "ORDER_HISTORY_URL": configs["ORDER_HISTORY_URL"],
+                "REFRESH_ACCESS_TOKEN_ENDPOINT": configs["REFRESH_ACCESS_TOKEN_ENDPOINT"],
+                "SEGMENT_KEY": configs["SEGMENT_KEY"],
+                "IGNORED_ERROR_REGEX": configs["IGNORED_ERROR_REGEX"],
+                "CREDENTIALS_BASE_URL": configs["CREDENTIALS_BASE_URL"],
+            },
+            "learning": configs.get("learning", dict()),
+            "account": configs.get("account", dict()),
+            "profile": configs.get("profile", dict()),
+        }
+
+        return Response(tennat_settings)
