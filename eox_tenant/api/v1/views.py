10a35fde25a365d3ce5d06b11eda18a2a386030c
black-redoc <joseb.twelve@gmail.com>
2022-04-19 23:01:08 -0500

    fix: fixed pylint warnings

--- eox_tenant/api/v1/views.py
+++ eox_tenant/api/v1/views.py
@@ -15,27 +15,27 @@ class MFESettingsView(APIView):
         configs = tenant.lms_configs
 
         common = {
-                "SITE_NAME": configs.get("SITE_NAME"),
-                "LOGO_IMAGE_URL": configs.get("LOGO_IMAGE__URL"),
-                "LOGO_TRADEMARK_URL": configs.get("LOGO_TRADEMARK_URL"),
-                "LOGO_WHITE_URL": configs.get("LOGO_WHITE_URL"),
-                "INFO_EMAIL": configs.get("INFO_EMAIL"),
-                "FAVICON_URL": configs.get("FAVICON_URL"),
-                "DISCOVERY_API_BASE_URL": configs.get("DISCOVERY_API_BASE_URL"),
-                "PUBLISHER_BASE_URL": configs.get("PUBLISHER_BASE_URL"),
-                "ECOMMERCE_BASE_URL": configs.get("ECOMMERCE_BASE_URL"),
-                "LEARNING_BASE_URL": configs.get("LEARNING_BASE_URL"),
-                "LMS_BASE_URL": configs["LMS_BASE_URL"],
-                "LOGIN_URL": configs.get("LOGIN_URL"),
-                "LOGOUT_URL": configs.get("LOGOUT_URL"),
-                "STUDIO_BASE_URL": configs.get("STUDIO_BASE_URL"),
-                "MARKETING_SITE_BASE_URL": configs.get("MARKETING_SITE_BASE_URL"),
-                "ORDER_HISTORY_URL": configs.get("ORDER_HISTORY_URL"),
-                "REFRESH_ACCESS_TOKEN_ENDPOINT": configs["REFRESH_ACCESS_TOKEN_ENDPOINT"],
-                "SEGMENT_KEY": configs.get("SEGMENT_KEY"),
-                "IGNORED_ERROR_REGEX": configs.get("IGNORED_ERROR_REGEX"),
-                "CREDENTIALS_BASE_URL": configs.get("CREDENTIALS_BASE_URL"),
-            }
+            "SITE_NAME": configs.get("SITE_NAME"),
+            "LOGO_IMAGE_URL": configs.get("LOGO_IMAGE__URL"),
+            "LOGO_TRADEMARK_URL": configs.get("LOGO_TRADEMARK_URL"),
+            "LOGO_WHITE_URL": configs.get("LOGO_WHITE_URL"),
+            "INFO_EMAIL": configs.get("INFO_EMAIL"),
+            "FAVICON_URL": configs.get("FAVICON_URL"),
+            "DISCOVERY_API_BASE_URL": configs.get("DISCOVERY_API_BASE_URL"),
+            "PUBLISHER_BASE_URL": configs.get("PUBLISHER_BASE_URL"),
+            "ECOMMERCE_BASE_URL": configs.get("ECOMMERCE_BASE_URL"),
+            "LEARNING_BASE_URL": configs.get("LEARNING_BASE_URL"),
+            "LMS_BASE_URL": configs["LMS_BASE_URL"],
+            "LOGIN_URL": configs.get("LOGIN_URL"),
+            "LOGOUT_URL": configs.get("LOGOUT_URL"),
+            "STUDIO_BASE_URL": configs.get("STUDIO_BASE_URL"),
+            "MARKETING_SITE_BASE_URL": configs.get("MARKETING_SITE_BASE_URL"),
+            "ORDER_HISTORY_URL": configs.get("ORDER_HISTORY_URL"),
+            "REFRESH_ACCESS_TOKEN_ENDPOINT": configs["REFRESH_ACCESS_TOKEN_ENDPOINT"],
+            "SEGMENT_KEY": configs.get("SEGMENT_KEY"),
+            "IGNORED_ERROR_REGEX": configs.get("IGNORED_ERROR_REGEX"),
+            "CREDENTIALS_BASE_URL": configs.get("CREDENTIALS_BASE_URL"),
+        }
 
         common = dict_filter(common)
 
@@ -49,5 +49,6 @@ class MFESettingsView(APIView):
 
         return Response(tennat_settings)
 
+
 def dict_filter(object: Dict) -> Dict:
-    return { key: value for key, value in object.items() if value is not None }
+    return {key: value for key, value in object.items() if value is not None}
