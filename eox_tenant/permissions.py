deb78a46e9bbe3742dae34c3c1c183e45360c2e7
Juan David Buitrago <juan.buitrago@edunext.co>
2022-08-08 13:29:06 -0500

    feat: catch build error

--- eox_tenant/permissions.py
+++ eox_tenant/permissions.py
@@ -22,16 +22,18 @@ def load_permissions():
     Helper method to load a custom permission on DB that will be use to control login permissions
     on tenants.
     """
-    # if settings.EOX_TENANT_LOAD_PERMISSIONS:
-    #     try:
-    #         content_type = ContentType.objects.get_for_model(get_user_model())
-    #         obj, created = Permission.objects.get_or_create(  # pylint: disable=unused-variable
-    #             codename=LOGIN_ALL_TENANTS_PERMISSION_CODENAME,
-    #             name='Can login to all eox-tenant Tenants',
-    #             content_type=content_type,
-    #         )
-    #     except ProgrammingError:
-    #         # This code runs when the app is loaded, if a migration has not been done a ProgrammingError exception
-    #         # is raised we are bypassing those cases to let migrations run smoothly.
-    #         pass
-    pass
+    try:
+        if settings.EOX_TENANT_LOAD_PERMISSIONS:
+            try:
+                content_type = ContentType.objects.get_for_model(get_user_model())
+                obj, created = Permission.objects.get_or_create(  # pylint: disable=unused-variable
+                    codename=LOGIN_ALL_TENANTS_PERMISSION_CODENAME,
+                    name='Can login to all eox-tenant Tenants',
+                    content_type=content_type,
+                )
+            except ProgrammingError:
+                # This code runs when the app is loaded, if a migration has not been done a ProgrammingError exception
+                # is raised we are bypassing those cases to let migrations run smoothly.
+                pass
+    except ProgrammingError:
+        pass
