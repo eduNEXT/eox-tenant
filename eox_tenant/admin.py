98d799564fa99b9962514df9ff157d828883cb27
Henrry Pulgarin <39854568+Henrrypg@users.noreply.github.com>
2022-08-31 13:12:52 -0500

    feat: change from branch (#1)

--- eox_tenant/admin.py
+++ eox_tenant/admin.py
@@ -12,8 +12,7 @@ from jsonfield.fields import JSONField
 
 from eox_tenant.models import Microsite, Route, TenantConfig, TenantOrganization
 from eox_tenant.widgets import JsonWidget
-HOLA = 'hola1
-'
+
 
 class MicrositeAdmin(admin.ModelAdmin):
     """
