3be01fe132f1dc1d5bc1cb7c584c50a216d4e10b
Maria Fernanda Magallanes Zubillaga <maria.magallanes@edunext.co>
2022-12-01 10:18:34 -0500

    feat: add pipeline step that filter render certificates by organization

--- eox_tenant/filters/pipeline.py
+++ eox_tenant/filters/pipeline.py
@@ -1,9 +1,10 @@
 """
 The pipeline module defines custom Filters functions that are used in openedx-filters.
 """
-from openedx_filters import PipelineStep
-
+from eox_tenant.organizations import get_organizations
 from eox_tenant.tenant_aware_functions.enrollments import filter_enrollments
+from openedx_filters import PipelineStep
+from openedx_filters.learning.filters import CertificateRenderStarted
 
 
 class FilterUserCourseEnrollmentsByTenant(PipelineStep):
@@ -27,3 +28,30 @@ class FilterUserCourseEnrollmentsByTenant(PipelineStep):
         """
         tenant_enrollments = filter_enrollments(context)
         return {"context": tenant_enrollments}
+
+class FilterRenderCertificatesByOrg(PipelineStep):
+    """
+    Stop certificate generation process raising a exception
+    if course org is different to tenant orgs.
+    Example usage:
+    Add the following configurations to your configuration file:
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.certificate.render.started.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "eox_tenant.filters.pipeline.FilterRenderCertificatesByOrg"
+                ]
+            }
+        }
+    """
+
+    def run_filter(self, context, custom_template, *args, **kwargs):  # pylint: disable=arguments-differ
+        """Pipeline step that stops the certificate render process if course org is different to tenant orgs."""
+        org_filter = get_organizations()
+        # breakpoint()
+        for org in org_filter:
+            if context.get("course_id","").startswith(f"course-v1:{org}+"):
+                return
+        raise CertificateRenderStarted.RenderAlternativeInvalidCertificate(
+            "You can't generate a certificate from this site.",
+        )
--- eox_tenant/tenant_wise/__init__.py
+++ eox_tenant/tenant_wise/__init__.py
@@ -10,7 +10,7 @@ import six
 from django.conf import settings
 
 from eox_tenant.constants import LMS_ENVIRONMENT
-from eox_tenant.tenant_wise.proxies import TenantGeneratedCertificateProxy, TenantSiteConfigProxy
+from eox_tenant.tenant_wise.proxies import TenantSiteConfigProxy
 
 
 def load_tenant_wise_overrides():
@@ -32,14 +32,6 @@ def load_tenant_wise_overrides():
                 proxy=TenantSiteConfigProxy
             )
 
-        if allowed_proxies.get('TenantGeneratedCertificateProxy') and LMS_ENVIRONMENT:
-            set_package_members_as_proxy(
-                package_name='lms.djangoapps.certificates',
-                model='GeneratedCertificate',
-                proxy=TenantGeneratedCertificateProxy,
-            )
-
-
 def set_as_proxy(modules, model, proxy):
     """
     Helper to patch a loaded module with a proxy object that has all the Tenant wise properties.
@@ -50,24 +42,3 @@ def set_as_proxy(modules, model, proxy):
     for module in modules:
         module = import_module(module)
         setattr(module, model, proxy)
-
-
-def set_package_members_as_proxy(package_name, model, proxy):
-    """
-    Get the modules from the given package and set every one as a proxy.
-
-    Args:
-        package_name: String with the package name, example 'lms.djangoapps.courseware'.
-        model: String The model name, example User.
-        proxy: Class of proxy model example TenantGeneratedCertificateProxy.
-    Returns:
-        None
-    """
-    package = import_module(package_name)
-    package_members = inspect.getmembers(package)
-
-    set_as_proxy(
-        modules=[member[1].__name__ for member in package_members if hasattr(member[1], model)],
-        model=model,
-        proxy=proxy
-    )
