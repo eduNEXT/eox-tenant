867571ac00a4868bad5e64d0a0938e59cd6ec797
Navin Karkera <navin@opencraft.com>
2023-05-16 11:07:26 +0530

    refactor: rename cmd edit_tenant_values to edit_microsite_values

--- eox_tenant/test/test_edit_microsite_values.py
+++ eox_tenant/test/test_edit_microsite_values.py
@@ -8,7 +8,7 @@ from eox_tenant.models import Microsite
 
 
 class EditTenantValuesTestCase(TestCase):
-    """ This class checks the command edit_tenant_values.py"""
+    """ This class checks the command edit_microsite_values.py"""
 
     def setUp(self):
         """This method creates Microsite objects in database"""
@@ -20,49 +20,49 @@ class EditTenantValuesTestCase(TestCase):
                 "NESTED_KEY": {"key": "value"},
             })
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_can_be_called(self, _):
         """Tests that we can actually run the command"""
-        call_command('edit_tenant_values')
+        call_command('edit_microsite_values')
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='n')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='n')
     def test_command_exec_confirmation_false(self, _):
         """Tests that when the confirmation returns other than 'y' we raise the abort error"""
         with self.assertRaises(CommandError):
-            call_command('edit_tenant_values')
+            call_command('edit_microsite_values')
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_exec_confirmation_add(self, _):
         """Tests that we can add a new key"""
-        call_command('edit_tenant_values', '--add', 'NEW_KEY', 'NEW_VALUE')
+        call_command('edit_microsite_values', '--add', 'NEW_KEY', 'NEW_VALUE')
         tenant = Microsite.objects.get(key='test')
         self.assertIn('NEW_KEY', tenant.values)
         self.assertEqual('NEW_VALUE', tenant.values.get('NEW_KEY'))
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_exec_confirmation_add_nested(self, _):
         """Tests that we can add a new nested key"""
-        call_command('edit_tenant_values', '--add', 'NEW_KEY.nested', 'NEW_VALUE')
+        call_command('edit_microsite_values', '--add', 'NEW_KEY.nested', 'NEW_VALUE')
         tenant = Microsite.objects.get(key='test')
         self.assertIn('nested', tenant.values.get('NEW_KEY'))
         self.assertEqual('NEW_VALUE', tenant.values.get('NEW_KEY').get('nested'))
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_exec_confirmation_delete(self, _):
         """Tests that we can remove a key"""
-        call_command('edit_tenant_values', '--delete', 'KEY')
+        call_command('edit_microsite_values', '--delete', 'KEY')
         tenant = Microsite.objects.get(key='test')
         self.assertNotIn('KEY', tenant.values)
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_exec_confirmation_delete_chain(self, _):
         """Tests that we can remove a nested key"""
-        call_command('edit_tenant_values', '--delete', 'NESTED_KEY.key')
+        call_command('edit_microsite_values', '--delete', 'NESTED_KEY.key')
         tenant = Microsite.objects.get(key='test')
         self.assertIn('NESTED_KEY', tenant.values)
         self.assertNotIn('key', tenant.values.get('NESTED_KEY'))
 
-    @patch('eox_tenant.management.commands.edit_tenant_values.input', return_value='y')
+    @patch('eox_tenant.management.commands.edit_microsite_values.input', return_value='y')
     def test_command_exec_confirmation_pattern(self, _):
         """Tests that we can affect only the sites defined by a pattern in their subdomain"""
         Microsite.objects.create(
@@ -73,7 +73,7 @@ class EditTenantValuesTestCase(TestCase):
                 "NESTED_KEY": {"key": "value"},
             })
 
-        call_command('edit_tenant_values', '--delete', 'KEY', '--pattern', 'second.test.prod.edunext.co')
+        call_command('edit_microsite_values', '--delete', 'KEY', '--pattern', 'second.test.prod.edunext.co')
         tenant1 = Microsite.objects.get(key='test')
         tenant2 = Microsite.objects.get(key='test2')
 
