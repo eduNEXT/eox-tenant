6f3020f0bfe0ce340d566c24f42608f113da76ee
Luis Felipe Castaño <78836902+luisfelipec95@users.noreply.github.com>
2023-07-06 10:27:15 -0500

    refactor: remove Darklang middleware because it is not being used (#186)

--- eox_tenant/edxapp_wrapper/backends/dark_lang_middleware_o_test_v1.py
+++
@@ -1,9 +0,0 @@
-"""
-DarkLangMiddleware Backend for tests.
-"""
-from unittest.mock import MagicMock
-
-
-def get_dark_lang_middleware():
-    """Backend to get the DarkLangMiddleware from openedx."""
-    return MagicMock()
