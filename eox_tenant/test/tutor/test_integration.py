1d12b8c7357156a80218f4cdc1ee978aa799e321
Felipe Montoya <felipe.montoya@edunext.co>
2024-03-07 13:01:27 -0500

    feat: moving to bare dir without src

--- eox_tenant/test/tutor/test_integration.py
+++ eox_tenant/test/tutor/test_integration.py
@@ -3,14 +3,28 @@ Test views file.
 """
 from django.test import TestCase
 from django.urls import reverse
+# from django.conf import settings
 
 
 class TutorIntegrationTestCase(TestCase):
     """
     """
 
-    def test_runs(self):
+    def test_runs_code(self):
         """
         """
-        "this test just passes"
+        "this test should just pass"
 
+    def test_current_settings_code_imports(self):
+        """
+        """
+        import eox_tenant.edxapp_wrapper.backends.oauth_dispatch_j_v1
+        import eox_tenant.edxapp_wrapper.backends.branding_api_l_v1
+        import eox_tenant.edxapp_wrapper.backends.certificates_module_i_v1
+        import eox_tenant.edxapp_wrapper.backends.site_configuration_module_i_v1
+        import eox_tenant.edxapp_wrapper.backends.theming_helpers_h_v1
+        import eox_tenant.edxapp_wrapper.backends.edx_auth_i_v1
+        import eox_tenant.edxapp_wrapper.backends.users_l_v1
+        import eox_tenant.edxapp_wrapper.backends.bearer_authentication_l_v1
+        import eox_tenant.edxapp_wrapper.backends.edxmako_l_v1
+        import eox_tenant.edxapp_wrapper.backends.util_h_v1
--- integration.sh
+++ integration.sh
@@ -1,3 +1,11 @@
-pip install -e .
-cd ../eox-tenant
-make test-integration-tutor
+# install the package in the edxapp env
+pip install -e ../eox-tenant
+
+# test that the commands for eox-tenant are being listed
+./manage.py lms help | grep eox
+
+#
+# cd ../eox-tenant
+# make test-integration-tutor
+make test-requirements
+python -Wd -m pytest -p no:randomly --ds=lms.envs.test /openedx/eox-tenant/eox_tenant/test/tutor
