868d53d9c2b7c28ec06272c65a6973f8bf732e91
Felipe Montoya <felipe.montoya@edunext.co>
2024-03-07 20:59:16 -0500

    feat: running command check and migrations

--- eox_tenant/test/tutor/integration.sh
+++ eox_tenant/test/tutor/integration.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 # install the package in the edxapp env
 pip install -e ../eox-tenant
 
-# test that the commands for eox-tenant are being listed
-./manage.py lms help | grep eox
+# test that the commands for eox-tenant are being listed at the lms
+is_command_installed=$(./manage.py lms help | grep eox | wc -l)
+if [ $is_command_installed == 0 ] ; then
+  echo 'package was not installed correctly'
+  exit -1;
+fi
+
+# run migrations
+./manage.py lms migrate eox_tenant
 
 # running the tests using the tutor settings
 make test-requirements
