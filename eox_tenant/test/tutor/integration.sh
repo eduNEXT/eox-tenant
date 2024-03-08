662d84c85ea1fde580f8e5cf565a778d5b48461a
Felipe Montoya <felipe.montoya@edunext.co>
2024-03-07 21:03:03 -0500

    feat: better comments in script

--- eox_tenant/test/tutor/integration.sh
+++ eox_tenant/test/tutor/integration.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # install the package in the edxapp env
+echo "Install package"
 pip install -e ../eox-tenant
 
 # test that the commands for eox-tenant are being listed at the lms
+echo "Tests django commands"
 is_command_installed=$(./manage.py lms help | grep eox | wc -l)
 if [ $is_command_installed == 0 ] ; then
   echo 'package was not installed correctly'
@@ -11,8 +13,13 @@ if [ $is_command_installed == 0 ] ; then
 fi
 
 # run migrations
+echo "Run migrations"
 ./manage.py lms migrate eox_tenant
 
-# running the tests using the tutor settings
+# install pytest
+echo "Install test-requirements"
 make test-requirements
+
+# running the tests using the tutor settings
+echo "Install test-requirements"
 pytest -s --ds=lms.envs.tutor.test /openedx/eox-tenant/eox_tenant/test/tutor
