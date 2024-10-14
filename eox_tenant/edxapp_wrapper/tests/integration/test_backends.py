1b65bd254173a084a578386b089916c54a1f3a46
magajh <maria.jaimes@edunext.co>
2024-10-14 11:26:25 -0400

    fix: run isort for backend tests

--- eox_tenant/edxapp_wrapper/tests/integration/test_backends.py
+++ eox_tenant/edxapp_wrapper/tests/integration/test_backends.py
@@ -8,11 +8,11 @@ def test_current_settings_code_imports():
     """
     Running this imports means that our backends import the right signature
     """
-    import eox_tenant.edxapp_wrapper.backends.oauth_dispatch_j_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.branding_api_l_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.site_configuration_module_i_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.theming_helpers_h_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.users_p_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.bearer_authentication_l_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.edxmako_l_v1  # isort:skip
-    import eox_tenant.edxapp_wrapper.backends.edx_auth_n_v1  # isort:skip
+    import eox_tenant.edxapp_wrapper.backends.bearer_authentication_l_v1
+    import eox_tenant.edxapp_wrapper.backends.branding_api_l_v1
+    import eox_tenant.edxapp_wrapper.backends.edx_auth_n_v1
+    import eox_tenant.edxapp_wrapper.backends.edxmako_l_v1
+    import eox_tenant.edxapp_wrapper.backends.oauth_dispatch_j_v1
+    import eox_tenant.edxapp_wrapper.backends.site_configuration_module_i_v1
+    import eox_tenant.edxapp_wrapper.backends.theming_helpers_h_v1
+    import eox_tenant.edxapp_wrapper.backends.users_p_v1
